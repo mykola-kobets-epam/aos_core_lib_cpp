b02951e05a79fda09e166410a87b67ff227f83d0
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2023-02-23 17:44:01 +0200

    [common] Implement wrapper for pthread primitives
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ include/aos/common/thread.hpp
@@ -0,0 +1,296 @@
+/*
+ * Copyright (C) 2023 Renesas Electronics Corporation.
+ * Copyright (C) 2023 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_THREAD_HPP_
+#define AOS_THREAD_HPP_
+
+#include <pthread.h>
+
+#include "aos/common/error.hpp"
+
+namespace aos {
+
+/**
+ * Aos thread.
+ */
+class Thread {
+public:
+    /**
+     * Constructs Aos thread instance.
+     *
+     * @param routine function to be called in thread.
+     * @param arg optional argument that is passed to the thread function.
+     */
+
+    template <typename T>
+    explicit Thread(T routine, void* arg = nullptr)
+        : mArg(arg)
+    {
+        static auto sRoutine = routine;
+
+        mAdapter = [](void* arg) -> void* {
+            sRoutine(arg);
+
+            return nullptr;
+        };
+    }
+
+    /**
+     * Runs thread function.
+     *
+     * @return Error.
+     */
+    Error Run() { return pthread_create(&mPThread, nullptr, mAdapter, mArg); }
+
+    /**
+     * Waits thread function is finished.
+     *
+     * @return Error.
+     */
+    Error Join() { return pthread_join(mPThread, nullptr); }
+
+private:
+    void* (*mAdapter)(void*);
+    void*     mArg;
+    pthread_t mPThread;
+
+    Thread(Thread const&) = delete;
+    Thread& operator=(Thread const&) = delete;
+};
+
+/**
+ * Aos mutex.
+ */
+class Mutex {
+public:
+    /**
+     * Constructs Aos mutex.
+     */
+    Mutex() { pthread_mutex_init(&mPMutex, nullptr); }
+
+    /**
+     * Destroys Aos mutex.
+     */
+    ~Mutex() { pthread_mutex_destroy(&mPMutex); }
+
+    /**
+     * Locks Aos mutex.
+     *
+     * @return Error.
+     */
+    Error Lock() { return pthread_mutex_lock(&mPMutex); }
+
+    /**
+     * Unlocks Aos mutex.
+     *
+     * @return Error.
+     */
+    Error Unlock() { return pthread_mutex_unlock(&mPMutex); }
+
+    /**
+     * Converts mutex to pthread_mutex_t pointer.
+     *
+     * @return pthread_mutex_t pointer.
+     */
+
+    explicit operator pthread_mutex_t*() { return &mPMutex; }
+
+private:
+    pthread_mutex_t mPMutex;
+
+    Mutex(Mutex const&) = delete;
+    Mutex& operator=(Mutex const&) = delete;
+};
+
+/**
+ * Aos lock guard.
+ */
+class LockGuard {
+public:
+    /**
+     * Creates lock guard instance.
+     *
+     * @param mutex mutex used to guard.
+     */
+    explicit LockGuard(Mutex& mutex)
+        : mMutex(mutex)
+    {
+        mError = mMutex.Lock();
+    }
+
+    /**
+     * Destroys lock guard instance.
+     */
+    ~LockGuard() { mMutex.Unlock(); }
+
+    /**
+     * Returns current lock guard error.
+     *
+     * @return Error.
+     */
+    Error GetError() { return mError; }
+
+private:
+    Mutex& mMutex;
+    Error  mError;
+
+    LockGuard(LockGuard const&) = delete;
+    LockGuard& operator=(LockGuard const&) = delete;
+};
+
+/**
+ * Aos unique lock.
+ */
+class UniqueLock {
+public:
+    /**
+     * Creates unique lock instance.
+     *
+     * @param mutex mutex used to guard.
+     */
+    explicit UniqueLock(Mutex& mutex)
+        : mMutex(mutex)
+        , mIsLocked(false)
+    {
+        mError = Lock();
+    }
+
+    /**
+     * Destroys unique lock instance.
+     */
+    ~UniqueLock()
+    {
+        if (mIsLocked) {
+            Unlock();
+        }
+    }
+
+    /**
+     * Locks unique lock instance.
+     */
+    Error Lock()
+    {
+        mError = mMutex.Lock();
+        if (!mError.IsNone()) {
+            return mError;
+        }
+
+        mIsLocked = true;
+
+        return mError;
+    }
+
+    /**
+     * Unlocks unique lock instance.
+     */
+    Error Unlock()
+    {
+        mError = mMutex.Unlock();
+        if (!mError.IsNone()) {
+            return mError;
+        }
+
+        mIsLocked = false;
+
+        return mError;
+    }
+
+    /**
+     * Returns current unique lock error.
+     *
+     * @return Error.
+     */
+    Error GetError() { return mError; }
+
+private:
+    Mutex& mMutex;
+    bool   mIsLocked;
+    Error  mError;
+
+    UniqueLock(UniqueLock const&) = delete;
+    UniqueLock& operator=(UniqueLock const&) = delete;
+};
+
+/**
+ * Aos conditional variable.
+ */
+class ConditionalVariable {
+public:
+    /**
+     * Creates conditional variable.
+     *
+     * @param mutex conditional variable mutex.
+     */
+    explicit ConditionalVariable(Mutex& mutex)
+        : mMutex(mutex)
+    {
+        mError = pthread_cond_init(&mCondVar, nullptr);
+    }
+
+    /**
+     * Destroys conditional variable.
+     */
+    ~ConditionalVariable() { pthread_cond_destroy(&mCondVar); }
+
+    /**
+     * Blocks the current thread until the condition variable is awakened.
+     *
+     * @return Error.
+     */
+    Error Wait() { return mError = pthread_cond_wait(&mCondVar, static_cast<pthread_mutex_t*>(mMutex)); }
+
+    /**
+     * Blocks the current thread until the condition variable is awakened and predicate condition is met.
+     *
+     * @return Error.
+     */
+    template <typename T>
+    Error Wait(T waitCondition)
+    {
+        while (!waitCondition()) {
+            auto err = Wait();
+            if (!err.IsNone()) {
+                return err;
+            }
+        }
+
+        return ErrorEnum::eNone;
+    }
+
+    /**
+     * Notifies one waiting thread.
+     *
+     * @return Error.
+     */
+    Error NotifyOne() { return mError = pthread_cond_signal(&mCondVar); }
+
+    /**
+     * Notifies all waiting thread.
+     *
+     * @return Error.
+     */
+    Error NotifyAll() { return mError = pthread_cond_broadcast(&mCondVar); }
+
+    /**
+     * Returns current conditional variable error.
+     *
+     * @return Error.
+     */
+    Error GetError() { return mError; }
+
+private:
+    Mutex&         mMutex;
+    pthread_cond_t mCondVar;
+    Error          mError;
+
+    ConditionalVariable(const ConditionalVariable&) = delete;
+    ConditionalVariable& operator=(ConditionalVariable const&) = delete;
+};
+
+} // namespace aos
+
+#endif
---
+++ tests/common/src/thread_test.cpp
@@ -0,0 +1,133 @@
+/*
+ * Copyright (C) 2023 Renesas Electronics Corporation.
+ * Copyright (C) 2023 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <gtest/gtest.h>
+
+#include "aos/common/thread.hpp"
+
+using namespace aos;
+
+constexpr auto cNumIteration = 100;
+
+class TestCalculator {
+public:
+    TestCalculator()
+        : mValue(0)
+        , mInc(1)
+    {
+    }
+
+    void   Inc() { mValue = mValue + mInc; }
+    void   SetIncrementer(int i) { mInc = i; }
+    int    GetResult() const { return mValue; }
+    Mutex& GetMutex() { return mMutex; }
+
+private:
+    int   mValue;
+    int   mInc;
+    Mutex mMutex;
+};
+
+static void calcDec(void* arg)
+{
+    auto calc = static_cast<TestCalculator*>(arg);
+
+    calc->SetIncrementer(-1);
+
+    for (auto i = 0; i < cNumIteration; i++) {
+        calc->Inc();
+    }
+}
+
+TEST(common, Thread)
+{
+    // Test thread, mutex, lock guard, lambda
+
+    TestCalculator calc;
+
+    Thread incThread([&calc](void*) {
+        for (auto i = 0; i < cNumIteration; i++) {
+            LockGuard lock(calc.GetMutex());
+            EXPECT_TRUE(lock.GetError().IsNone());
+
+            calc.Inc();
+
+            usleep(1000);
+        }
+    });
+    Thread distThread([&](void*) {
+        for (auto i = 0; i < cNumIteration; i++) {
+            LockGuard lock(calc.GetMutex());
+
+            calc.SetIncrementer(0);
+
+            usleep(1000);
+
+            calc.SetIncrementer(1);
+        }
+    });
+
+    EXPECT_TRUE(incThread.Run().IsNone());
+    EXPECT_TRUE(distThread.Run().IsNone());
+
+    EXPECT_TRUE(incThread.Join().IsNone());
+    EXPECT_TRUE(distThread.Join().IsNone());
+
+    EXPECT_EQ(calc.GetResult(), cNumIteration);
+
+    // Test static function
+
+    Thread decThread(calcDec, &calc);
+
+    EXPECT_TRUE(decThread.Run().IsNone());
+    EXPECT_TRUE(decThread.Join().IsNone());
+
+    EXPECT_EQ(calc.GetResult(), 0);
+}
+
+TEST(common, CondVar)
+{
+    Mutex               mutex;
+    ConditionalVariable condVar(mutex);
+    auto                ready = false;
+    auto                processed = false;
+
+    Thread worker([&](void*) {
+        UniqueLock lock(mutex);
+        EXPECT_TRUE(lock.GetError().IsNone());
+
+        EXPECT_TRUE(condVar.Wait([&] { return ready; }).IsNone());
+
+        processed = true;
+
+        EXPECT_TRUE(lock.Unlock().IsNone());
+
+        EXPECT_TRUE(condVar.NotifyOne().IsNone());
+    });
+
+    EXPECT_TRUE(worker.Run().IsNone());
+
+    {
+        LockGuard lock(mutex);
+        EXPECT_TRUE(lock.GetError().IsNone());
+
+        ready = true;
+    }
+
+    EXPECT_TRUE(condVar.NotifyOne().IsNone());
+
+    {
+        LockGuard lock(mutex);
+        EXPECT_TRUE(lock.GetError().IsNone());
+
+        EXPECT_TRUE(condVar.Wait([&] { return processed; }).IsNone());
+    }
+
+    EXPECT_TRUE(ready);
+    EXPECT_TRUE(processed);
+    EXPECT_TRUE(worker.Join().IsNone());
+}
