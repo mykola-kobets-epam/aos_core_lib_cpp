42d154609afcdd25503ddea544402215abe935b7
Mykola Kobets <mykola_kobets@epam.com>
2023-11-01 16:39:13 +0200

    [iam] Implement CertModule/CertHandler
    Signed-off-by: Mykola Kobets <mykola_kobets@epam.com>

--- src/iam/certhandler/certhandler.cpp
+++ src/iam/certhandler/certhandler.cpp
@@ -16,6 +16,142 @@ namespace certhandler {
  * Public
  **********************************************************************************************************************/
 
+CertHandler::CertHandler()
+{
+}
+
+Error CertHandler::RegisterModule(CertModule& module)
+{
+    LockGuard lock(mMutex);
+
+    LOG_INF() << "Register module: type=" << module.GetCertType();
+
+    return AOS_ERROR_WRAP(mModules.PushBack(&module));
+}
+
+Error CertHandler::GetCertTypes(Array<StaticString<cCertTypeLen>>& certTypes)
+{
+    LockGuard lock(mMutex);
+
+    LOG_DBG() << "Get all registered IAM certificate types";
+
+    for (const auto module : mModules) {
+        auto err = certTypes.PushBack(module->GetCertType());
+        if (!err.IsNone()) {
+            return AOS_ERROR_WRAP(err.Value());
+        }
+    }
+
+    return Error::Enum::eNone;
+}
+
+Error CertHandler::SetOwner(const String& certType, const String& password)
+{
+    LockGuard lock(mMutex);
+
+    LOG_DBG() << "Set owner. type=" << certType;
+
+    auto* module = FindModule(certType);
+    if (module == nullptr) {
+        return AOS_ERROR_WRAP(Error::Enum::eNotFound);
+    }
+
+    return module->SetOwner(password);
+}
+
+Error CertHandler::Clear(const String& certType)
+{
+    LockGuard lock(mMutex);
+
+    LOG_DBG() << "Clear all certificates. type=" << certType;
+
+    auto* module = FindModule(certType);
+    if (module == nullptr) {
+        return AOS_ERROR_WRAP(Error::Enum::eNotFound);
+    }
+
+    return module->Clear();
+}
+
+Error CertHandler::CreateKey(
+    const String& certType, const Array<uint8_t>& subject, const String& password, Array<uint8_t>& pemCSR)
+{
+    LockGuard lock(mMutex);
+
+    LOG_DBG() << "Create key. type=" << certType << ", subject=" << subject;
+
+    auto* module = FindModule(certType);
+    if (module == nullptr) {
+        return AOS_ERROR_WRAP(Error::Enum::eNotFound);
+    }
+
+    auto key = module->CreateKey(password);
+    if (!key.mError.IsNone()) {
+        return key.mError;
+    }
+
+    return module->CreateCSR(subject, *key.mValue, pemCSR);
+}
+
+Error CertHandler::ApplyCertificate(const String& certType, const Array<uint8_t>& cert, CertInfo& info)
+{
+    LockGuard lock(mMutex);
+
+    LOG_DBG() << "Apply cert. type=" << certType << ", url=" << info.mCertURL;
+
+    auto* module = FindModule(certType);
+    if (module == nullptr) {
+        return AOS_ERROR_WRAP(Error::Enum::eNotFound);
+    }
+
+    return module->ApplyCert(cert, info);
+}
+
+Error CertHandler::GetCertificate(
+    const String& certType, const Array<uint8_t>& issuer, const Array<uint8_t>& serial, CertInfo& resCert)
+{
+    LockGuard lock(mMutex);
+
+    LOG_DBG() << "Get certificate. type=" << certType << ", serial=" << serial;
+
+    auto* module = FindModule(certType);
+    if (module == nullptr) {
+        return AOS_ERROR_WRAP(Error::Enum::eNotFound);
+    }
+
+    return module->GetCertificate(issuer, serial, resCert);
+}
+
+Error CertHandler::CreateSelfSignedCert(const String& certType, const String& password)
+{
+    LockGuard lock(mMutex);
+
+    LOG_DBG() << "Create self signed cert. type=" << certType;
+
+    auto* module = FindModule(certType);
+    if (module == nullptr) {
+        return AOS_ERROR_WRAP(Error::Enum::eNotFound);
+    }
+
+    return module->CreateSelfSignedCert(password);
+}
+
+CertHandler::~CertHandler()
+{
+    LOG_DBG() << "Close certificate handler";
+}
+
+/***********************************************************************************************************************
+ * Private
+ **********************************************************************************************************************/
+
+CertModule* CertHandler::FindModule(const String& certType) const
+{
+    auto module = mModules.Find([certType](CertModule* module) { return module->GetCertType() == certType; });
+
+    return !module.mError.IsNone() ? nullptr : *module.mValue;
+}
+
 } // namespace certhandler
 } // namespace iam
 } // namespace aos
