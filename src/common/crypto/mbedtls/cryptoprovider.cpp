9c4376d1b89c6bc5969b6f103f81bfcaeea95278
Mykola Solianko <mykola_solianko@epam.com>
2024-01-25 21:19:00 +0200

    [crypto] Implement convert DN to string
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>

--- src/common/crypto/mbedtls/cryptoprovider.cpp
+++ src/common/crypto/mbedtls/cryptoprovider.cpp
@@ -110,6 +110,72 @@ aos::Error MbedTLSCryptoProvider::ASN1EncodeDN(const aos::String& commonName, ao
     return result.Resize(len);
 }
 
+aos::Error MbedTLSCryptoProvider::ASN1DecodeDN(const aos::Array<uint8_t>& dn, aos::String& result)
+{
+    int                  ret {};
+    size_t               len;
+    const unsigned char* end        = dn.Get() + dn.Size();
+    unsigned char*       currentPos = const_cast<aos::Array<uint8_t>&>(dn).Get();
+
+    if ((ret = mbedtls_asn1_get_tag(&currentPos, end, &len, MBEDTLS_ASN1_CONSTRUCTED | MBEDTLS_ASN1_SEQUENCE)) != 0) {
+        return AOS_ERROR_WRAP(ret);
+    }
+
+    const unsigned char* sequenceEnd = currentPos + len;
+
+    while (currentPos < sequenceEnd) {
+        if ((ret = mbedtls_asn1_get_tag(&currentPos, sequenceEnd, &len, MBEDTLS_ASN1_CONSTRUCTED | MBEDTLS_ASN1_SET))
+            != 0) {
+            return AOS_ERROR_WRAP(ret);
+        }
+
+        const unsigned char* setEnd = currentPos + len;
+
+        if ((ret = mbedtls_asn1_get_tag(&currentPos, setEnd, &len, MBEDTLS_ASN1_CONSTRUCTED | MBEDTLS_ASN1_SEQUENCE))
+            != 0) {
+            return AOS_ERROR_WRAP(ret);
+        }
+
+        const unsigned char* seqEnd = currentPos + len;
+
+        if ((ret = mbedtls_asn1_get_tag(&currentPos, seqEnd, &len, MBEDTLS_ASN1_OID)) != 0) {
+            return AOS_ERROR_WRAP(ret);
+        }
+
+        auto oid {aos::Array<uint8_t> {currentPos, len}};
+
+        auto err = GetOIDString(oid, result);
+        if (err != aos::ErrorEnum::eNone) {
+            return err;
+        }
+
+        currentPos += len;
+
+        unsigned char tag = *currentPos;
+
+        if (tag != MBEDTLS_ASN1_UTF8_STRING && tag != MBEDTLS_ASN1_PRINTABLE_STRING) {
+            return aos::ErrorEnum::eInvalidArgument;
+        }
+
+        if ((ret = mbedtls_asn1_get_tag(&currentPos, seqEnd, &len, tag)) != 0) {
+            return AOS_ERROR_WRAP(ret);
+        }
+
+        result.Insert(
+            result.end(), reinterpret_cast<const char*>(currentPos), reinterpret_cast<const char*>(currentPos) + len);
+        result.Append(", ");
+
+        currentPos += len;
+    }
+
+    if (!result.IsEmpty()) {
+        // Remove the last two characters (", ")
+        result.Resize(result.Size() - 2);
+    }
+
+    return aos::ErrorEnum::eNone;
+}
+
 /***********************************************************************************************************************
  * Private
  **********************************************************************************************************************/
@@ -212,6 +278,28 @@ aos::Error MbedTLSCryptoProvider::GetX509CertExtensions(aos::crypto::x509::Certi
 
         next = next->next;
     }
+
+    return aos::ErrorEnum::eNone;
+}
+
+aos::Error MbedTLSCryptoProvider::GetOIDString(aos::Array<uint8_t>& oid, aos::String& result)
+{
+    mbedtls_asn1_buf oidBuf;
+    oidBuf.p   = oid.Get();
+    oidBuf.len = oid.Size();
+
+    const char* shortName {};
+
+    int ret = mbedtls_oid_get_attr_short_name(&oidBuf, &shortName);
+    if (ret != 0) {
+        return AOS_ERROR_WRAP(ret);
+    }
+
+    if (shortName == nullptr) {
+        return aos::ErrorEnum::eNone;
+    }
+
+    result.Append(shortName).Append("=");
 
     return aos::ErrorEnum::eNone;
 }
