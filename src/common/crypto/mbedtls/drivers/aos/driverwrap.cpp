888ebc640e64fdd1efa624bbfd8a3690a3074174
Mykola Solianko <mykola_solianko@epam.com>
2024-01-25 18:37:43 +0200

    [crypto] Implement EC in driver
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>

--- src/common/crypto/mbedtls/drivers/aos/driverwrap.cpp
+++ src/common/crypto/mbedtls/drivers/aos/driverwrap.cpp
@@ -7,6 +7,7 @@
 
 #include <mbedtls/asn1write.h>
 #include <mbedtls/base64.h>
+#include <mbedtls/oid.h>
 #include <mbedtls/pem.h>
 #include <mbedtls/pk.h>
 #include <mbedtls/rsa.h>
@@ -88,6 +89,120 @@ static int ExportRsaPublicKeyToDer(
     MBEDTLS_ASN1_CHK_ADD(len, mbedtls_asn1_write_tag(&c, data, MBEDTLS_ASN1_CONSTRUCTED | MBEDTLS_ASN1_SEQUENCE));
 
     memmove(data, c, len);
+    *dataLength = len;
+
+    cleanup();
+
+    return 0;
+}
+
+static aos::Pair<psa_ecc_family_t, size_t> FindPsaECGroupByOID(const aos::Array<uint8_t>& oid)
+{
+    for (int i = MBEDTLS_ECP_DP_NONE; i < MBEDTLS_ECP_DP_MAX; ++i) {
+        const char* group_oid;
+        size_t      group_oid_len;
+
+        if (mbedtls_oid_get_oid_by_ec_grp(static_cast<mbedtls_ecp_group_id>(i), &group_oid, &group_oid_len) == 0) {
+            aos::Array<uint8_t> known_oid_buf((uint8_t*)group_oid, group_oid_len);
+
+            if (oid == known_oid_buf) {
+                switch (i) {
+                case MBEDTLS_ECP_DP_SECP192R1:
+                    return {PSA_ECC_FAMILY_SECP_R1, 192};
+                case MBEDTLS_ECP_DP_SECP224R1:
+                    return {PSA_ECC_FAMILY_SECP_R1, 224};
+                case MBEDTLS_ECP_DP_SECP256R1:
+                    return {PSA_ECC_FAMILY_SECP_R1, 256};
+                case MBEDTLS_ECP_DP_SECP384R1:
+                    return {PSA_ECC_FAMILY_SECP_R1, 384};
+                case MBEDTLS_ECP_DP_SECP521R1:
+                    return {PSA_ECC_FAMILY_SECP_R1, 521};
+                case MBEDTLS_ECP_DP_SECP192K1:
+                    return {PSA_ECC_FAMILY_SECP_K1, 192};
+                case MBEDTLS_ECP_DP_SECP224K1:
+                    return {PSA_ECC_FAMILY_SECP_K1, 224};
+                case MBEDTLS_ECP_DP_SECP256K1:
+                    return {PSA_ECC_FAMILY_SECP_K1, 256};
+                case MBEDTLS_ECP_DP_CURVE25519:
+                    return {PSA_ECC_FAMILY_MONTGOMERY, 255};
+                case MBEDTLS_ECP_DP_CURVE448:
+                    return {PSA_ECC_FAMILY_MONTGOMERY, 448};
+                case MBEDTLS_ECP_DP_BP256R1:
+                    return {PSA_ECC_FAMILY_BRAINPOOL_P_R1, 256};
+                case MBEDTLS_ECP_DP_BP384R1:
+                    return {PSA_ECC_FAMILY_BRAINPOOL_P_R1, 384};
+                case MBEDTLS_ECP_DP_BP512R1:
+                    return {PSA_ECC_FAMILY_BRAINPOOL_P_R1, 512};
+
+                default:
+                    return {PSA_ECC_FAMILY_SECP_R1, 0};
+                }
+            }
+        }
+    }
+
+    return {PSA_ECC_FAMILY_SECP_R1, 0};
+}
+
+static aos::Pair<aos::Error, mbedtls_ecp_group_id> FindECPGroupByOID(const aos::Array<uint8_t>& oid)
+{
+    for (int i = MBEDTLS_ECP_DP_NONE; i < MBEDTLS_ECP_DP_MAX; ++i) {
+        const char* group_oid;
+        size_t      group_oid_len;
+
+        if (mbedtls_oid_get_oid_by_ec_grp(static_cast<mbedtls_ecp_group_id>(i), &group_oid, &group_oid_len) == 0) {
+            aos::Array<uint8_t> known_oid_buf((uint8_t*)group_oid, group_oid_len);
+
+            if (oid == known_oid_buf) {
+                return {aos::ErrorEnum::eNone, static_cast<mbedtls_ecp_group_id>(i)};
+            }
+        }
+    }
+
+    return {aos::ErrorEnum::eNotFound, MBEDTLS_ECP_DP_NONE};
+}
+
+static int ExportECPublicKeyToDer(
+    const aos::crypto::ECDSAPublicKey& ecKey, uint8_t* data, size_t dataSize, size_t* dataLength)
+{
+    auto curveParameters = FindECPGroupByOID(ecKey.GetECParamsOID());
+    if (!curveParameters.mFirst.IsNone()) {
+        return -1;
+    }
+
+    mbedtls_ecp_group grp;
+    mbedtls_ecp_group_init(&grp);
+    mbedtls_ecp_point Q;
+    mbedtls_ecp_point_init(&Q);
+
+    auto cleanup = [&]() {
+        mbedtls_ecp_group_free(&grp);
+        mbedtls_ecp_point_free(&Q);
+    };
+
+    auto ret = mbedtls_ecp_group_load(&grp, curveParameters.mSecond);
+    if (ret != 0) {
+        cleanup();
+
+        return ret;
+    }
+
+    const aos::Array<uint8_t>& point = ecKey.GetECPoint();
+
+    if ((ret = mbedtls_ecp_point_read_binary(&grp, &Q, point.Get(), point.Size())) != 0) {
+        cleanup();
+
+        return ret;
+    }
+
+    size_t len = 0;
+
+    if ((ret = mbedtls_ecp_point_write_binary(&grp, &Q, MBEDTLS_ECP_PF_UNCOMPRESSED, &len, data, dataSize)) != 0) {
+        cleanup();
+
+        return ret;
+    }
+
     *dataLength = len;
 
     cleanup();
@@ -185,8 +300,19 @@ psa_status_t aos_get_builtin_key(psa_drv_slot_number_t slotNumber, psa_key_attri
 
                 break;
 
-            case aos::crypto::KeyTypeEnum::eECDSA:
-                return PSA_ERROR_NOT_SUPPORTED;
+            case aos::crypto::KeyTypeEnum::eECDSA: {
+                psa_set_key_algorithm(attributes, PSA_ALG_ECDSA(PSA_ALG_SHA_256));
+                auto oid = static_cast<const aos::crypto::ECDSAPublicKey&>(key.mPrivKey->GetPublic()).GetECParamsOID();
+                auto curveParameters = FindPsaECGroupByOID(oid);
+                if (curveParameters.mSecond == 0) {
+                    return PSA_ERROR_NOT_SUPPORTED;
+                }
+
+                psa_set_key_type(attributes, PSA_KEY_TYPE_ECC_KEY_PAIR(curveParameters.mFirst));
+                psa_set_key_bits(attributes, curveParameters.mSecond);
+
+                break;
+            }
 
             default:
                 return PSA_ERROR_NOT_SUPPORTED;
@@ -214,7 +340,8 @@ psa_status_t aos_signature_sign_hash(const psa_key_attributes_t* attributes, con
     for (auto& key : sBuiltinKeys) {
         if (key.mKeyId == psa_get_key_id(attributes)) {
             switch (key.mPrivKey->GetPublic().GetKeyType().GetValue()) {
-            case aos::crypto::KeyTypeEnum::eRSA: {
+            case aos::crypto::KeyTypeEnum::eRSA:
+            case aos::crypto::KeyTypeEnum::eECDSA: {
                 aos::crypto::SignOptions options;
                 options.mHash = aos::crypto::HashEnum::eSHA256;
 
@@ -230,9 +357,6 @@ psa_status_t aos_signature_sign_hash(const psa_key_attributes_t* attributes, con
 
                 return PSA_SUCCESS;
             }
-
-            case aos::crypto::KeyTypeEnum::eECDSA:
-                return PSA_ERROR_NOT_SUPPORTED;
 
             default:
                 return PSA_ERROR_NOT_SUPPORTED;
@@ -263,8 +387,16 @@ psa_status_t aos_export_public_key(const psa_key_attributes_t* attributes, const
                 return PSA_SUCCESS;
             }
 
-            case aos::crypto::KeyTypeEnum::eECDSA:
-                return PSA_ERROR_NOT_SUPPORTED;
+            case aos::crypto::KeyTypeEnum::eECDSA: {
+                auto ret
+                    = ExportECPublicKeyToDer(static_cast<const aos::crypto::ECDSAPublicKey&>(key.mPrivKey->GetPublic()),
+                        data, data_size, data_length);
+                if (ret != 0) {
+                    return PSA_ERROR_NOT_SUPPORTED;
+                }
+
+                return PSA_SUCCESS;
+            }
 
             default:
                 return PSA_ERROR_NOT_SUPPORTED;
