7973080646331f6c3855dea3900f56c439b3a2d8
Mykola Kobets <mykola_kobets@epam.com>
2025-06-10 20:39:48 +0300

    Revert "[wip] Revert cgroup dir change"
    This reverts commit bc049c4ee1d62f7d518479f1aeee0450e590e1da.

--- src/sm/launcher/instance.cpp
+++ src/sm/launcher/instance.cpp
@@ -18,6 +18,17 @@ namespace aos::sm::launcher {
 
 namespace {
 static const char* const cBindEtcEntries[] = {"nsswitch.conf", "ssl"};
+
+StaticString<cFilePathLen> CreateServiceName(const String& instanceID) {
+    StaticString<cFilePathLen> path;
+
+    path = "aos-service@";
+    path += instanceID;
+    path += ".service";
+
+    return path;
+}
+
 }
 
 StaticAllocator<Instance::cAllocatorSize, Instance::cNumAllocations> Instance::sAllocator {};
@@ -550,7 +561,7 @@ Error Instance::CreateLinuxSpec(
     runtimeSpec.mProcess->mUser.mUID = mInstanceInfo.mUID;
     runtimeSpec.mProcess->mUser.mGID = mService.mGID;
 
-    runtimeSpec.mLinux->mCgroupsPath = fs::JoinPath(cCgroupsPath, mInstanceID);
+    runtimeSpec.mLinux->mCgroupsPath = fs::JoinPath(cCgroupsPath, CreateServiceName(mInstanceID));
 
     runtimeSpec.mRoot->mPath     = fs::JoinPath(mRuntimeDir, cRootFSDir);
     runtimeSpec.mRoot->mReadonly = false;
